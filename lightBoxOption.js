89f403ba33302aa06103060764811e25904bd328
Samvel <svksam@yahoo.com>
2020-03-23 21:53:40 -0700

    updated some test and pictures

--- lightBoxOption.js
+++ lightBoxOption.js
@@ -1,8 +1,8 @@
 //Controls light box options
 function lightBoxOption() {
 	lightbox.option({
-		resizeDuration: 200,
-		fadeDuration: 200,
+		resizeDuration: 100,
+		fadeDuration: 100,
 		wrapAround: true,
 		disableScrolling: true
 	});
