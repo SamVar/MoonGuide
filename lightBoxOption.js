3b6cbea783ddb8e211549f9f875c6fccc054710e
Samvel <svksam@yahoo.com>
2020-05-19 20:33:26 -0700

    new

--- lightBoxOption.js
+++ lightBoxOption.js
@@ -7,4 +7,22 @@ function lightBoxOption() {
 		disableScrolling: true,
 		alwaysShowNavOnTouchDevices: true,
 	});
+
+	//Checks if device is mobile or desktop
+	let isMobile = navigator.userAgent.match(
+		/(iPhone|iPod|iPad|Android|webOS|BlackBerry|IEMobile|Opera Mini)/i
+	);
+
+	//If device is mobile position 200px from top
+	if (isMobile) {
+		lightbox.option({
+			positionFromTop: 150,
+		});
+	}
+	//If device is Desktop position is 50px from top
+	else {
+		lightbox.option({
+			positionFromTop: 50,
+		});
+	}
 }
